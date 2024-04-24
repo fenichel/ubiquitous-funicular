c2c80a287cd1210d4a738b8537c38ab25df83ab7
Rachel Fenichel <fenichel@google.com>
2024-04-23 18:04:58 -0700

    feat: add shortcut to jump to root of stack

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ State as of April 23:
 - The workspace and blocks are from running `npx @blockly/create-package app`
 - renderer: zelos
 - cursor: line cursor,
-- Controls:
-  - Basic keyboard nav controls, but with WASD key behaviour moved to the arrow keys
-  - Pressing `a` calls the `announce` shortcut, which prints the type of the currently selected node to the `Announcer Text` area on the left side.
+
+Controls:
+- Basic keyboard nav controls, but with WASD key behaviour moved to the arrow keys
+- `a`: Calls the `announce` shortcut, which prints the type of the currently selected node to the `Announcer Text` area on the left side.
+- `r`: Moves the cursor to the root of the current stack 
 
 
 Possible next steps:
--- keyboard-experiment/src/extended_navigation_controller.js
+++ keyboard-experiment/src/extended_navigation_controller.js
@@ -1,7 +1,8 @@
 
 import { NavigationController } from '@blockly/keyboard-navigation';
 import { Constants } from '@blockly/keyboard-navigation';
-import * as Blockly from 'blockly';
+import { ASTNode, ShortcutRegistry } from 'blockly';
+import { utils as BlocklyUtils } from 'blockly';
 
 export class ExtendedNavigationController extends NavigationController {
     // override
@@ -27,29 +28,82 @@ export class ExtendedNavigationController extends NavigationController {
             },
         };
 
-        Blockly.ShortcutRegistry.registry.register(announceShortcut);
-        Blockly.ShortcutRegistry.registry.addKeyMapping(
-            Blockly.utils.KeyCodes.A,
+        ShortcutRegistry.registry.register(announceShortcut);
+        ShortcutRegistry.registry.addKeyMapping(
+            BlocklyUtils.KeyCodes.A,
+            announceShortcut.name,
+        );
+    }
+
+    registerJumpToRoot() {
+        const jumpShortcut = {
+            name: 'Jump to root of current stack',
+            preconditionFn: (workspace) => {
+                return (true);
+            },
+            // Print out the type of the current node.
+            callback: (workspace) => {
+                const announcer = document.getElementById('announcer');
+                const cursor = workspace.getCursor();
+                const curNode = cursor.getCurNode();
+                const curBlock = curNode.getSourceBlock();
+                if (curBlock) { 
+                    const rootBlock = curBlock.getRootBlock();
+                    const stackNode = ASTNode.createStackNode(rootBlock);
+                    cursor.setCurNode(stackNode);
+                    announcer.innerText = 'jumped to root';
+                } else {
+                    announcer.innerText = 'could not jump to root';
+                }
+                return true;
+            },
+        };
+
+        ShortcutRegistry.registry.register(jumpShortcut);
+        ShortcutRegistry.registry.addKeyMapping(
+            BlocklyUtils.KeyCodes.R,
+            jumpShortcut.name,
+        );
+    }
+    registerAnnounce() {
+        const announceShortcut = {
+            name: 'Announce',
+            preconditionFn: (workspace) => {
+                return (true);
+            },
+            // Print out the type of the current node.
+            callback: (workspace) => {
+                const announcer = document.getElementById('announcer');
+                const cursor = workspace.getCursor();
+                announcer.innerText = cursor.getCurNode().getType();
+                return true;
+            },
+        };
+
+        ShortcutRegistry.registry.register(announceShortcut);
+        ShortcutRegistry.registry.addKeyMapping(
+            BlocklyUtils.KeyCodes.A,
             announceShortcut.name,
         );
     }
     registerAddOns() {
         this.registerAnnounce();
+        this.registerJumpToRoot();
     }
 
     // Remap to use arrow keys instead.
     remapDefaults() {
-        Blockly.ShortcutRegistry.registry.removeAllKeyMappings(Constants.SHORTCUT_NAMES.OUT);
-        Blockly.ShortcutRegistry.registry.addKeyMapping(Blockly.utils.KeyCodes.LEFT, Constants.SHORTCUT_NAMES.OUT);
+        ShortcutRegistry.registry.removeAllKeyMappings(Constants.SHORTCUT_NAMES.OUT);
+        ShortcutRegistry.registry.addKeyMapping(BlocklyUtils.KeyCodes.LEFT, Constants.SHORTCUT_NAMES.OUT);
 
-        Blockly.ShortcutRegistry.registry.removeAllKeyMappings(Constants.SHORTCUT_NAMES.IN);
-        Blockly.ShortcutRegistry.registry.addKeyMapping(Blockly.utils.KeyCodes.RIGHT, Constants.SHORTCUT_NAMES.IN);
+        ShortcutRegistry.registry.removeAllKeyMappings(Constants.SHORTCUT_NAMES.IN);
+        ShortcutRegistry.registry.addKeyMapping(BlocklyUtils.KeyCodes.RIGHT, Constants.SHORTCUT_NAMES.IN);
 
-        Blockly.ShortcutRegistry.registry.removeAllKeyMappings(Constants.SHORTCUT_NAMES.PREVIOUS);
-        Blockly.ShortcutRegistry.registry.addKeyMapping(Blockly.utils.KeyCodes.UP, Constants.SHORTCUT_NAMES.PREVIOUS);
+        ShortcutRegistry.registry.removeAllKeyMappings(Constants.SHORTCUT_NAMES.PREVIOUS);
+        ShortcutRegistry.registry.addKeyMapping(BlocklyUtils.KeyCodes.UP, Constants.SHORTCUT_NAMES.PREVIOUS);
 
-        Blockly.ShortcutRegistry.registry.removeAllKeyMappings(Constants.SHORTCUT_NAMES.NEXT);
-        Blockly.ShortcutRegistry.registry.addKeyMapping(Blockly.utils.KeyCodes.DOWN, Constants.SHORTCUT_NAMES.NEXT);
+        ShortcutRegistry.registry.removeAllKeyMappings(Constants.SHORTCUT_NAMES.NEXT);
+        ShortcutRegistry.registry.addKeyMapping(BlocklyUtils.KeyCodes.DOWN, Constants.SHORTCUT_NAMES.NEXT);
     }
 };
 
